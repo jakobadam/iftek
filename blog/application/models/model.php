46e5daffd9ac61d0f26e7797ec01d7c120fad728
Jakob Dam <jakob.a.dam@gmail.com>
2011-07-11 19:25:46 +0200

    initial commit

---
+++ blog/application/models/model.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Generisk Model klasse.
+ * 
+ * Indeholder genbrugelig funktionalitet for alle modeller.
+ */
+class Model{
+    
+    var $id;
+    var $created_at;
+    var $updated_at;
+
+}
+?>
---
+++ blog/application/models/post.php
@@ -0,0 +1,11 @@
+<?php
+
+class Post extends Model{
+    
+    var $body;
+    var $title;
+    var $is_published;
+    var $user_id;
+    
+}
+?>
