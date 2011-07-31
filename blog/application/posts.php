a2a8cd6d5f1fdbbab160402c3a5915fd54ec3a0c
Jakob Dam <jakob.a.dam@gmail.com>
2011-07-31 15:32:36 +0200

    blog fixture for testing

--- blog/application/posts.php
+++ blog/application/posts.php
@@ -1,26 +1,37 @@
 <?php
 
-include("base_controller.php");
+include_once("base_controller.php");
+include_once("models/post.php");
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 $posts = array(
-    array(
-        id=>1,
+    new Post(array(
+    	id=>1,
         title=>"title 1",
         body=>"the body",
         user_id=>1,
         created_at=>"0000-00-00"
-        ),
-    array(
+        )),
+    new Post(array(
         id=>2,
         title=>"title 2",
         body=>"the body 2",
         user_id=>1,
         created_at=>"0000-00-00"
-    ));
+    )),
+      new Post(array(
+      id=>3,
+        title=>"title 1",
+        body=>"the body",
+        user_id=>1,
+        created_at=>"0000-00-00"
+        )),
+          new Post(array(
+      id=>4,
+        title=>"title 1",
+        body=>"the body",
+        user_id=>1,
+        created_at=>"0000-00-00"
+        )));
 
 echo(render("posts.html", array(posts=>$posts)));
 
