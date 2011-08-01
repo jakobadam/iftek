7f843a21cfefac3713dbada0b9dc83c00824cbcd
Jakob Dam <jakob.a.dam@gmail.com>
2011-08-01 08:56:04 +0200

    is logged in.

--- blog/base_controller.php
+++ blog/base_controller.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once 'libs/Twig/Autoloader.php';
+require_once('libs/Twig/Autoloader.php');
+require_once('models/user.php');
 
 Twig_Autoloader::register();
 
@@ -40,9 +41,9 @@ function url_root(){
  * som vi har brug for hver gang.
  */
 function populate_context($context){
-	if(array_key_exists('email', $_SESSION)){
+	if(array_key_exists('user_id', $_SESSION)){
 	 	// TODO: get user by email	
-	 	$user = array('email'=>'foo@example.com');
+	 	$user = new User(array('email'=>'foo@example.com', 'id'=>1));
 	 	$context['user'] = $user;
 	}
 	$context['messages'] = get_flashes();
@@ -55,12 +56,16 @@ function populate_context($context){
  * Omdireger til login hvis bruger ikke er logget ind.
  */
 function login_required(){
-	if(!array_key_exists('email', $_SESSION)){
+	if(!is_logged_in()){
 		// TODO: redirect to where the user is coming from.
 		flash_error('Hey, du skal være logget ind!');
 	 	echo(render('login_form.html'));
 	 	die();
 	}	
+}
+
+function is_logged_in(){
+    return array_key_exists('user_id', $_SESSION);
 }
 
 function render($template, $context=array()){
