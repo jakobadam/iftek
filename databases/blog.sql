fe40bb8c6d2483e9290449b18df8c173dd3e6476
Jakob Dam <jakob.a.dam@gmail.com>
2011-12-01 14:12:46 +0100

    removed auto increment.

--- databases/blog.sql
+++ databases/blog.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `user_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_posts_users` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` datetime NOT NULL,
   `password` varchar(20) CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB;
 
 
 ALTER TABLE `posts`
