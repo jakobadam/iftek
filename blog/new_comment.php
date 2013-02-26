c2b4f7f292b07c9ae250598da72376cd075a1ae1
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2013-02-26 18:42:59 +0100

    new_comment.php with assignments

---
+++ blog/new_comment.php
@@ -0,0 +1,30 @@
+<?php
+    require("controller.php");
+    require("db.php");
+
+    if($_SERVER['REQUEST_METHOD'] != 'POST') {
+        echo("Denne handling er kun lovlig med POST");
+        die();
+    }
+
+    login_required();
+	// NOTE: user_id sættes ved login, og er derfor altid tilstede her.
+    $user_id = $_SESSION['user_id'];
+
+    $comment = ''; // OPGAVE: Læs værdien af kommentaren ud fra POST
+    $post_id = $_GET['id'];
+    
+    // OPGAVE: Tilpas nedenstående SQL, så det passer til din comments tabel
+    $sql = "INSERT INTO comments (post_id, user_id, comment, is_removed) VALUES (?,?,?,0)";
+    $stm = db_query($sql, array($post_id, $user_id, $comment));
+
+    // Vis fejl hvis kommentar ikke kunne indsættes
+    if (!$stm) {
+        die('Fejl ved indsættelse: ' . mysql_error());
+    }
+
+    flash('Ny kommentar oprettet!');
+    
+    // OPGAVE: Hvad gør nedenstående linje, og hvorfor?
+    header('Location: post.php?id=' . $post_id);
+?>
