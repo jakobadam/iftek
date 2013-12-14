a526e46203a45c0360a5c685519e2af0e0e4a846
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2013-12-14 23:42:21 +0100

    removed special danish letters.

--- blog/conf/config.php
+++ blog/conf/config.php
@@ -1,12 +1,12 @@
 <?php    
     define('DB_URL', 'localhost');
-    define('DB_USER', 'dit_brugernavn'); // indsæt brugernavn mellem ''
-    define('DB_PWD', 'dit_password'); // indsæt koden mellem ''
-    define('DB_NAME', 'dit_brugernavn.blog'); // ændre brugernavn
+    define('DB_USER', 'dit_brugernavn');
+    define('DB_PWD', 'dit_password');
+    define('DB_NAME', 'dit_brugernavn.blog');
 
     if(DB_USER == '' || DB_PWD == '' || DB_NAME == ''){
         header('Content-type: text/html; charset=utf-8');
-        print('Husk du skal sætte navnet på database brugeren, koden til databasen, og navnet på den i conf/config.php');
+        print('Husk du skal angive navnet paa database brugeren, koden til databasen, og navnet paa den i conf/config.php');
         die();
     }
 ?>
